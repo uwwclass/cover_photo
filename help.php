27af37df2dbb59d00c6085e96c1cb20c0a568094
ybnomsebo <ybnomsebo@gmail.com>
2015-11-01 19:30:32 -0800

    Create help.php

---
+++ help.php
@@ -0,0 +1,41 @@
+<?php
+/*
+ *      OSCLass – software for creating and publishing online classified
+ *                           advertising platforms
+ *
+ *                        Copyright (C) 2010 OSCLASS
+ *
+ *       This program is free software: you can redistribute it and/or
+ *     modify it under the terms of the GNU Affero General Public License
+ *     as published by the Free Software Foundation, either version 3 of
+ *            the License, or (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *         WITHOUT ANY WARRANTY; without even the implied warranty of
+ *        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *             GNU Affero General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Affero General Public
+ * License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+   ?>
+   <div  style="border: 1px solid #ccc; background: #eee; padding-left:20px;">
+   <h2>Profile Picture  Help</h2><br />
+   
+   <h3>What is Cover Photo  Plugin?</h3>
+   
+   <p>&raquo; Cover Photo plugin allows user to upload  cover photo for his profile.</p>
+   <p>&raquo; Admin can manage the users uploaded cover photos like appprove , deny , delete </p>
+   
+   <h3>How to Use Cover Photo  Plugin?</h3>
+   <p>&raquo;To Display User Cover Photo in user public profile add the following line of code by editing your theme file user-publick-profile.php  : </p>
+   <pre> &lt;?php cover_photo_show(); ?&gt; </pre>
+   
+   <h3>How to Use Cover Photo upload option</h3>
+   <p>&raquo;To Display User Cover Photo upload option in user dashboard add the following line of code by editing your theme file user-dashboard.php  : </p>
+   <pre> &lt;?php cover_photo_upload(); ?&gt; </pre>
+   
+   
+   <h3>Can I Change the CSS ?</h3>
+   <p>&raquo; Yes. You can add your own css.</p>
+   </div>
